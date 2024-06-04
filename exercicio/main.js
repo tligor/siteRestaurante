bdce72f39043a56b386570a0c6c3429ca27ed587
Igor Santana <santanna.ig@hotmail.com>
2024-06-04 16:41:53 -0300

    adicionado pasta de exercicio com novas implementações

---
+++ exercicio/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        autoplayTimeout: 10000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        autoplaySpeed: 800,
+        slideSpeed: 500,
+    });
+  });
